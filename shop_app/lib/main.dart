46079defeb8355ef5433ddb9eacda9bc37e67d06
Mohamed BENTOUHAMI <mbentouhami@adflux.net>
2023-02-15 11:24:19 +0100

    add products overview screen

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../screens/products_overview_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,21 +13,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'MyShop',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('MyShop')),
-      body: const Center(
-        child: Text('Let\'s build a shop'),
-      ),
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.purple,
+            secondary: Colors.deepOrange,
+          ),
+          fontFamily: 'Lato'),
+      home: ProductsOverviewScreen(),
     );
   }
 }
